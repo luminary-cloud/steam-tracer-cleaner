26861e450683cff9a4e0066e5736fa904f1acfd0
luminary-cloud <68704042+luminary-cloud@users.noreply.github.com>
2026-05-26 19:12:49 +0200

    Bump version to 1.0.0 and update icon

--- core/version.hpp
+++ core/version.hpp
@@ -2,7 +2,7 @@
 
 namespace stc::core {
 
-inline constexpr const char* kAppVersion = "0.1.4";
-inline constexpr const char* kAppVersionTag = "v0.1.4";
+inline constexpr const char* kAppVersion = "1.0.0";
+inline constexpr const char* kAppVersionTag = "v1.0.0";
 
 }  // namespace stc::core
